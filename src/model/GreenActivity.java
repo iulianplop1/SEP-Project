4a9bf996fd6844c45d22e5541f2e190072176d6a
arsenieBlb <bilba.arsenie@gmail.com>
2025-12-01 17:17:20 +0100

    ass

--- src/model/GreenActivity.java
+++ src/model/GreenActivity.java
@@ -3,5 +3,46 @@ package model;
 public class GreenActivity
 {
   private String activityName;
-  private int name;
+  private int points;
+  public GreenActivity(String activityName, int points)
+  {
+    this.activityName=activityName;
+    this.points=points;
+  }
+
+  public void setActivityName(String activityName)
+  {
+    this.activityName = activityName;
+  }
+
+  public void setPoints(int points)
+  {
+    this.points = points;
+  }
+
+  public int getPoints()
+  {
+    return points;
+  }
+
+  public String getActivityName()
+  {
+    return activityName;
+  }
+  public boolean equals(Object obj)
+  {
+    if(obj==null||getClass()!=obj.getClass())
+      return false;
+    GreenActivity other = (GreenActivity) obj;
+    return this.activityName.equals(other.activityName) &&
+        points==other.points;
+  }
+  public String toString()
+  {
+    return "The activity "+activityName+" has "+points+ " points";
+  }
+  public GreenActivity copy()
+  {
+    return new GreenActivity(activityName, points);
+  }
 }
