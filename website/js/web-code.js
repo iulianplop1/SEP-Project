1d61ce1b5646148d451b6ecfd4195a6d051b45e7
batorgabora <batorgabora@gmail.com>
2025-12-08 14:39:09 +0100

    push

--- website/js/web-code.js
+++ website/js/web-code.js
@@ -26,11 +26,60 @@ fetch('json/greenActivityList.json')
     .catch(error => console.error('Error fetching JSON:', error));
 
 
+
+
+const outputDiv = document.getElementById('output');
 let trades = [];
 
 async function loadTrades() {
-  const res = await fetch('./json/tradesList.json');
-  trades = await res.json();  // ← SAVED HERE
+    const res = await fetch('json/tradesList.json');
+    trades = await res.json();  // JSON loaded
+
+    outputDiv.innerHTML = trades
+        .map(t => `
+            <div>
+                <strong>${t.tradename}</strong>  [${t.points}] by \t
+                 ${t.seller.firstname} ${t.seller.lastname} - 
+                 "${t.description}"
+            </div>
+        `)
+        .join('');
 }
 
 loadTrades();
+
+fetch('json/greenGoalList.json')
+    .then(response => response.json())
+    .then(data => {
+        const tbody = document.getElementById("tableBody1");
+
+        for (let i = 0; i < data.length; i++) {
+
+            const goal = data[i];
+
+            // Format nested date object
+            const d = goal.description;
+
+            // Create table row
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+                <td>${goal.goalName}</td>
+                <td>${goal.RequiredPoints}</td>
+                <td>${goal.description}</td>
+            `;
+
+            t.appendChild(row);
+        }
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
+    
+
+fetch('json/VillageDescription.json')
+    .then(response => response.json())
+    .then(data => {
+        // Assuming your JSON is a single object with a key like 'text'
+        const villageDescription = data.text; // Adjust based on actual JSON keys
+        document.getElementById("DescriptionJson").innerHTML = `<tr><td>${villageDescription}</td></tr>`;
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
