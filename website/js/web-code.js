ad939af2b6dd4df368e0876fd29f4996680541b4
batorgabora <batorgabora@gmail.com>
2025-12-10 10:49:18 +0100

    push

--- website/js/web-code.js
+++ website/js/web-code.js
@@ -1,30 +1,3 @@
-/*fetch('json/greenActivityList.json')
-    .then(response => response.json())
-    .then(data => {
-        const tbody = document.getElementById("tableBody");
-
-        for (let i = 0; i < data.length; i++) {
-
-            const activity = data[i];
-
-            // Format nested date object
-            const d = activity.date;
-            const formattedDate = `${d.day}/${d.month}/${d.year}`;
-
-            // Create table row
-            const row = document.createElement("tr");
-
-            row.innerHTML = `
-                <td>${activity.activityName}</td>
-                <td>${activity.points}</td>
-                <td>${formattedDate}</td>
-            `;
-
-            tbody.appendChild(row);
-        }
-    })
-    .catch(error => console.error('Error fetching JSON:', error));*/
-
     fetch('json/greenActivityList.json')
     .then(response => response.json())
     .then(data => {
